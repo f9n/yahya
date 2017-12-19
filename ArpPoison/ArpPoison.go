6113f3bf9dff6d4dbe1ff8624953a009f0dc7c35
pleycpl <h4rvey@protonmail.com>
2017-12-19 20:07:09 +0000

    added gopacket arpscan example

--- ArpPoison/ArpPoison.go
+++ ArpPoison/ArpPoison.go
@@ -1,8 +1,15 @@
 package ArpPoison
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"log"
+	"net"
+	"sync"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
@@ -15,6 +22,104 @@ func AttackArpPoison(ifname string) {
 
 func DetectArpPoison(ifname string) {
 	fmt.Println("Detect")
-	version := pcap.Version()
-	fmt.Println(version)
+	// Get a list of all interfaces.
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	for _, iface := range ifaces {
+		wg.Add(1)
+		// Start up a scan on each interface.
+		go func(iface net.Interface) {
+			defer wg.Done()
+			if err := scan(&iface); err != nil {
+				log.Printf("interface %v: %v", iface.Name, err)
+			}
+			fmt.Println(iface.Name)
+		}(iface)
+	}
+	// Wait for all interfaces' scans to complete.  They'll try to run
+	// forever, but will stop on an error, so if we get past this Wait
+	// it means all attempts to write have failed.
+	wg.Wait()
+}
+
+// scan scans an individual interface's local network for machines using ARP requests/replies.
+//
+// scan loops forever, sending packets out regularly.  It returns an error if
+// it's ever unable to write a packet.
+func scan(iface *net.Interface) error {
+	// We just look for IPv4 addresses, so try to find if the interface has one.
+	var addr *net.IPNet
+	if addrs, err := iface.Addrs(); err != nil {
+		return err
+	} else {
+		for _, a := range addrs {
+			if ipnet, ok := a.(*net.IPNet); ok {
+				if ip4 := ipnet.IP.To4(); ip4 != nil {
+					addr = &net.IPNet{
+						IP:   ip4,
+						Mask: ipnet.Mask[len(ipnet.Mask)-4:],
+					}
+					break
+				}
+			}
+		}
+	}
+	// Sanity-check that the interface has a good address.
+	if addr == nil {
+		return errors.New("no good IP network found")
+	} else if addr.IP[0] == 127 {
+		return errors.New("skipping localhost")
+	} else if addr.Mask[0] != 0xff || addr.Mask[1] != 0xff {
+		return errors.New("mask means network is too large")
+	}
+	log.Printf("Using network range %v for interface %v", addr, iface.Name)
+
+	// Open up a pcap handle for packet reads/writes.
+	handle, err := pcap.OpenLive(iface.Name, 65536, true, pcap.BlockForever)
+	if err != nil {
+		return err
+	}
+	defer handle.Close()
+
+	// Start up a goroutine to read in packet data.
+	stop := make(chan struct{})
+	go readARP(handle, iface, stop)
+	defer close(stop)
+	var ch int
+	fmt.Scanf("%d", &ch)
+	return nil
+}
+
+// readARP watches a handle for incoming ARP responses we might care about, and prints them.
+//
+// readARP loops until 'stop' is closed.
+func readARP(handle *pcap.Handle, iface *net.Interface, stop chan struct{}) {
+	src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
+	in := src.Packets()
+	for {
+		var packet gopacket.Packet
+		select {
+		case <-stop:
+			return
+		case packet = <-in:
+			arpLayer := packet.Layer(layers.LayerTypeARP)
+			if arpLayer == nil {
+				continue
+			}
+			arp := arpLayer.(*layers.ARP)
+			if arp.Operation != layers.ARPReply || bytes.Equal([]byte(iface.HardwareAddr), arp.SourceHwAddress) {
+				// This is a packet I sent.
+				fmt.Println("Hiiii")
+				continue
+			}
+			// Note:  we might get some packets here that aren't responses to ones we've sent,
+			// if for example someone else sends US an ARP request.  Doesn't much matter, though...
+			// all information is good information :)
+			log.Printf("IP %v is at %v", net.IP(arp.SourceProtAddress), net.HardwareAddr(arp.SourceHwAddress))
+		}
+	}
 }
